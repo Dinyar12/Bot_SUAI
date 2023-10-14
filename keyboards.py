9dd9bd8c5cd0c36839edf0832fdc63809c6b9816
Alexander992992 <tyushev.sasha@mail.ru>
2023-10-14 14:46:15 +0300

    language complete

--- keyboards.py
+++ keyboards.py
@@ -1,18 +1,17 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import json
 
-language="en"
+language="ru"
 
 with open("localizations/"+language+".json",encoding="UTF-8") as file:
     text = json.load(file)
     
 def create_start_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup()
-    btn1 = InlineKeyboardButton(text["start"]["route"], callback_data='search')
-    btn2 = InlineKeyboardButton(text["start"]["nearest"], callback_data='round_place')
-    btn3 = InlineKeyboardButton(text["start"]["excursions"], callback_data='round_performance')
-    markup.row(btn1)
-    markup.row(btn2)
+    btn1 = InlineKeyboardButton(text["start_btn"]["route"], callback_data='search')
+    btn2 = InlineKeyboardButton(text["start_btn"]["nearest"], callback_data='round_place')
+    btn3 = InlineKeyboardButton(text["start_btn"]["excursions"], callback_data='round_performance')
+    markup.row(btn1,btn2)
     markup.row(btn3)
     return markup
 
@@ -25,16 +24,15 @@ def create_menu_places_keyboard() -> InlineKeyboardMarkup:
     btn4 = InlineKeyboardButton(text["places"]["exhibitions"]+'🖼', callback_data='exhibition')
     btn5 = InlineKeyboardButton(text["places"]["monuments"]+'🗿', callback_data='sites')
     btn6 = InlineKeyboardButton(text["places"]["events"]+'📆', callback_data='performance')
-    markup.row(btn1, btn2)
-    markup.row(btn3, btn4)
-    markup.row(btn5, btn6)
+    markup.row(btn1, btn2, btn3)
+    markup.row(btn4, btn5, btn6)
     return markup
 
 
 def create_information_keyboard() -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup()
-    btn1 = InlineKeyboardButton('Информация', callback_data='info')
-    btn2 = InlineKeyboardButton('Проложить маршрут', callback_data='rout')
+    btn1 = InlineKeyboardButton(text["information"]["info"], callback_data='info')
+    btn2 = InlineKeyboardButton(text["information"]["route"], callback_data='rout')
     markup.row(btn1)
     markup.row(btn2)
     return markup
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from keyboards import *
 
 bot = TeleBot('6666010835:AAGG7yhkJis2P0KVl3k72lXx5jMl6UMKi2M')
 
+language="ru"
+
+with open("localizations/"+language+".json",encoding="UTF-8") as file:
+    text = json.load(file)
 
 def path(message:types.Message):
     bot.send_message(message.chat.id, 'Horray!')
@@ -10,7 +14,7 @@ def path(message:types.Message):
 
 def near_places(message):
     murkup = create_menu_places_keyboard()
-    bot.send_message(message.chat.id, 'Выберите категорию мест, которую Вы хотите посетить', reply_markup=murkup)
+    bot.send_message(message.chat.id, text["other"]["category"], reply_markup=murkup)
 
 
 def near_performance(message):
@@ -19,60 +23,57 @@ def near_performance(message):
 
 def restaurants(message):
     markup = create_information_keyboard()
-    bot.send_message(message.chat.id, 'Вариант1:', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Вариант2:', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Вариант3:', reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v1"], reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v2"], reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v3"], reply_markup=markup)
 
 
 def hotel(message):
     markup = create_information_keyboard()
-    bot.send_message(message.chat.id, 'Вариант1:', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Вариант2:', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Вариант3:', reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v1"], reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v2"], reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v2"], reply_markup=markup)
 
 
 def museam(message):
     markup = create_information_keyboard()
-    bot.send_message(message.chat.id, 'Вариант1:', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Вариант2:', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Вариант3:', reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v1"], reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v2"], reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v2"], reply_markup=markup)
 
 
 def sites(message):
     markup = create_information_keyboard()
-    bot.send_message(message.chat.id, 'Вариант1:', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Вариант2:', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Вариант3:', reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v1"], reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v2"], reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v2"], reply_markup=markup)
 
 
 def exhibition(message):
     markup = create_information_keyboard()
-    bot.send_message(message.chat.id, 'Вариант1:', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Вариант2:', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Вариант3:', reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v1"], reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v2"], reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v2"], reply_markup=markup)
 
 
 def performance(message):
     markup = create_information_keyboard()
-    bot.send_message(message.chat.id, 'Вариант1:', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Вариант2:', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Вариант3:', reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v1"], reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v2"], reply_markup=markup)
+    bot.send_message(message.chat.id, text["other"]["v2"], reply_markup=markup)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = create_start_keyboard()
     bot.send_message(message.chat.id,
-                     'Бот может предоставлять информацию о популярных туристических направлениях, а также о местных достопримечательностях и мероприятиях. Он может также отвечать на вопросы пользователей о том, что посмотреть и чем заняться в том или ином месте.'
-                     'Бот может помогать спланировать маршрут путешествия, бронированием билетов и гостиниц. Он может предлагать различные варианты маршрутов, основанные на интересах пользователя, и помогать с поиском и бронированием билетов и гостиниц по лучшим ценам.'
-                     'Бот может предоставлять персонализированные рекомендации и советы пользователям. Он может также помогать с решением проблем, которые могут возникнуть во время путешествия.\n'
-                     '<b>Travel Bot - это полезный инструмент для путешественников, желающих узнать больше о том или ином месте и спланировать свое путешествие.</b>',
+                     text["other"]["start_text"],
                      reply_markup=markup, parse_mode='html')
 
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.chat.id, 'Контакты техподдежки: +7 950 891-51-26')
+    bot.send_message(message.chat.id, text["other"]["Contact"]+'+7 950 891-51-26')
 
 
 d = {"search": path,
